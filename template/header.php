2bc4688c202ac75dc363b70e9084abad9d2f0f3b
vdisoo0053 <vdisoo0053@SNSOO1EP01VM013.saojose.sc.senai.br>
2015-12-01 09:08:37 -0200

    criaçao header

--- template/header.php
+++ template/header.php
@@ -1,7 +1,47 @@
 <html>
     <head>
-        <meta charset="utf-8"> 
-        <title><php echo $title; ?></title>
-        <link type="text/css" href="/template
-   </body>
+        <meta charset="UTF-8">
+        <title><?php echo $title; ?></title>
+        <link type="text/css" rel="stylesheet" href="template/style/app.css" 
+    </head>
 </html>
+
+
+app.css:
+*  {
+    margin: 0;
+    padding: 0;
+}
+.center{
+    width: 980px;
+    margin: 100px auto;
+}
+input[type="text"] {
+    width : 100%;
+    padding: 10px;
+    padding-left: 0;
+    padding-right: 0;
+    border: 1px solid #CCC;
+    background: #FFF;
+    transition: linear 0.9;
+}
+
+input[type="text"]:focus {
+    box-shadow: 0px 0px 14px 0px rgba(50, 50, 50, 0, 75);
+}
+
+input[type="submit"] {
+    padding: 10px;
+    border: 1px solid #CCC;
+    background: #CCC;
+}
+
+input[type="submit"]:hover {
+  background: #333;    
+  color: FFF;
+}
+
+input[type="submit"]:active{
+  background: #333;    
+  color: FFF;
+}
