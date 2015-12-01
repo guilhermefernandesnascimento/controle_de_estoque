e90cacd18bd90ae7f3a73e5734da702ee056b410
vdisoo0053 <vdisoo0053@SNSOO1EP01VM013.saojose.sc.senai.br>
2015-12-01 09:05:25 -0200

    criaçao footer

---
+++ template/footer.php
@@ -0,0 +1,50 @@
+ </body>
+</html>
+
+header:
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title><?php echo $title; ?></title>
+        <link type="text/css" rel="stylesheet" href="template/style/app.css" 
+    </head>
+</html>
+
+*  {
+    margin: 0;
+    padding: 0;
+}
+.center{
+    width: 980px;
+    margin: 100px auto;
+}
+input[type="text"] {
+    width : 100%;
+    padding: 10px;
+    padding-left: 0;
+    padding-right: 0;
+    border: 1px solid #CCC;
+    background: #FFF;
+    transition: linear 0.9;
+}
+
+input[type="text"]:focus {
+    box-shadow: 0px 0px 14px 0px rgba(50, 50, 50, 0, 75);
+}
+
+input[type="submit"] {
+    padding: 10px;
+    border: 1px solid #CCC;
+    background: #CCC;
+}
+
+input[type="submit"]:hover {
+  background: #333;    
+  color: FFF;
+}
+
+input[type="submit"]:active{
+  background: #333;    
+  color: FFF;
+}
+
